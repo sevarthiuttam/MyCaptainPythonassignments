8738896bae62aadd48dfea4b8fdae045539cd28d
Sevarthi Uttam Kumar <135597253+sevarthiuttam@users.noreply.github.com>
2023-06-16 19:55:03 +0530

    Add files via upload

--- positivenumbers.py
+++ positivenumbers.py
@@ -1,5 +1,6 @@
-s= int(input())
-e= int(input())
-for n in range(s,e+ 1):
-    if n>=0:
+start=int(input("Enter the starting number of the range: "))
+end=int(input("Enter the ending number of the range: "))
+print("Positive numbers in the range are:")
+for n in range(start,end+1):
+    if n>0:
         print(n)
